e564c78a563e3a5c4f31ed4dfd75f17beed8d70f
fael <rafaelzapico.ti@gmail.com>
2023-05-08 18:45:37 -0300

    começando o projeto

---
+++ src/main/java/br/com/rafael/Transcender/TranscenderApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.Transcender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafael/Transcender/TranscenderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.Transcender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
