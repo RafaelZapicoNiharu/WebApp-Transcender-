ceeffaad29a1dad45bf60371d61444f27f36c4be
fael <rafaelzapico.ti@gmail.com>
2023-05-17 18:49:09 -0300

    arrumei uns bagui

--- src/main/java/br/com/rafael/Transcender/model/Avaliacao.java
+++ src/main/java/br/com/rafael/Transcender/model/Avaliacao.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 
 @Data
@@ -15,6 +14,9 @@ import javax.persistence.ManyToOne;
 @Builder
 @Entity
 public class Avaliacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private ENotaEmpresa ava;
 
     @ManyToOne(optional = true)
--- src/main/java/br/com/rafael/Transcender/model/Habilidade.java
+++ src/main/java/br/com/rafael/Transcender/model/Habilidade.java
@@ -6,9 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
@@ -17,6 +15,9 @@ import java.util.List;
 @Builder
 @Entity
 public class Habilidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
     private String nome;
 }
