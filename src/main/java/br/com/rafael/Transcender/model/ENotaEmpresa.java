0d1f33d5fa4f4d162a40733d4613f3be990c6a3e
rafaelZapico <rafaelzapico00@gmail.com>
2023-06-07 11:16:51 -0300

    descarte de algumas ideias antigas

--- src/main/java/br/com/rafael/Transcender/model/ENotaEmpresa.java
+++
@@ -1,6 +0,0 @@
-package br.com.rafael.Transcender.model;
-
-public enum ENotaEmpresa {
-
-    MUITORUIM, RUIM, NORMAL, BOA, MUITOBOA;
-}
---
+++ src/main/java/br/com/rafael/Transcender/model/descartados/ENotaEmpresa.java
@@ -0,0 +1,6 @@
+//package br.com.rafael.Transcender.model;
+//
+//public enum ENotaEmpresa {
+//
+//    MUITORUIM, RUIM, NORMAL, BOA, MUITOBOA;
+//}
