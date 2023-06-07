09231dc0efcb56f1c0856707a9fea05808fadf04
rafaelZapico <rafaelzapico00@gmail.com>
2023-06-07 11:20:10 -0300

    descarte de algumas ideias antigas

---
+++ src/main/java/br/com/rafael/Transcender/model/descartados/AvaliacaoDao.java
@@ -0,0 +1,10 @@
+//package br.com.rafael.Transcender.model.dao;
+//
+//
+//
+//import br.com.rafael.Transcender.model.Avaliacao;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface AvaliacaoDao extends JpaRepository<Avaliacao, Integer> {
+//}
