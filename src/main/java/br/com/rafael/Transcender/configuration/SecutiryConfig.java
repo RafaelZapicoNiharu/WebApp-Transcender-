a6ef059c93c9d7dff5cae910c6e1656e434c0bcf
rafaelZapico <rafaelzapico00@gmail.com>
2023-06-13 15:20:08 -0300

    implementando melhorias securityconfig

--- src/main/java/br/com/rafael/Transcender/configuration/SecutiryConfig.java
+++ src/main/java/br/com/rafael/Transcender/configuration/SecutiryConfig.java
@@ -1,9 +1,13 @@
 package br.com.rafael.Transcender.configuration;
 
+import br.com.rafael.Transcender.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,15 +18,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableGlobalAuthentication() //lembrar de perguntar ao daves oq esses fazem pq eu esqueci :)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }//criptografia das senhas
+
+    @Autowired
+    UserService uServ; //injeção do userservice
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(uServ) //aqui vai estar pegando o user  pra logar
+                .passwordEncoder(passwordEncoder());
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //aqui a gente configurou quais telas serão acessadas pelos determinados usuarios do sistema
+        //ou seja, as permissões que cad um vai ter, e também os que serão publicos
         http.authorizeRequests()
                 .antMatchers("/home","/cadastro","/h2/**","/css/**","/img/**","/webjars/**").permitAll()
                 .antMatchers("/mod/**").hasRole("ADMINISTRADOR")
-
+                .antMatchers("/company/**").hasRole("EMPRESA")
+                .antMatchers("/user/**").hasRole("EMPRESA")
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .loginPage("/login")
@@ -35,27 +57,6 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("user")
-                .password(passwordEncoder().encode("123"))
-                .roles("PESSOA")
 
-                .and()
-                .withUser("admin")
-                .password(passwordEncoder().encode("123"))
-                .roles("ADMINISTRADOR")
 
-                .and()
-                .withUser("empresa")
-                .password(passwordEncoder().encode("123"))
-                .roles("EMPRESA");
-
-    }
 }
