69496b8595d7768c28e4a15123d76d73cb690926
rafaelZapico <rafaelzapico00@gmail.com>
2023-06-18 12:42:29 -0300

    cadastro de usuarios !

--- src/main/java/br/com/rafael/Transcender/controller/HomeController.java
+++ src/main/java/br/com/rafael/Transcender/controller/HomeController.java
@@ -1,14 +1,25 @@
 package br.com.rafael.Transcender.controller;
 
+import br.com.rafael.Transcender.model.Habilidade;
+import br.com.rafael.Transcender.model.Usuario;
+import br.com.rafael.Transcender.service.HabilidadeService;
+import br.com.rafael.Transcender.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = {"/home","/"})
 public class HomeController {
+
+    @Autowired
+    UserService usuarioservice;
 
     @GetMapping("")
     public String pageHome(Model model) {
@@ -29,7 +40,18 @@ public class HomeController {
     @GetMapping("/cadastro")
     public String pageCadastro(Model model) {
 
+        model.addAttribute("usuario",new Usuario()); //adiciona usuario no model
+
         return "cadastro";
+    }
+    @PostMapping("/cadastro/save")
+    public String pageSaveUsuario(@ModelAttribute Usuario usuario,
+                                     Model model, Authentication auth){
+
+        usuarioservice.saveUsuario(usuario); //aqui utiliza o service para poder salvar
+        // o usuario passada pelo formulario para o banco, atraves do save
+
+        return "redirect:/login";
     }
 
 }
