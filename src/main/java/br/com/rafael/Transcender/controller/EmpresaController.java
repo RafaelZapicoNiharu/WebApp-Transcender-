5a74fcdb2ddaa44009f10f88043426d9f0df9007
fael <rafaelzapico.ti@gmail.com>
2023-06-21 19:47:38 -0300

    qualidade: unificação dos diferentes tipo de perfil

--- src/main/java/br/com/rafael/Transcender/controller/EmpresaController.java
+++ src/main/java/br/com/rafael/Transcender/controller/EmpresaController.java
@@ -58,6 +58,6 @@ public class EmpresaController {
 
 
 
-        return "perfilEmpresa";
+        return "perfil";
     }
 }
--- src/main/java/br/com/rafael/Transcender/controller/UserController.java
+++ src/main/java/br/com/rafael/Transcender/controller/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 
 
 
-        return "perfilUsuario";
+        return "perfil";
     }
 
 
